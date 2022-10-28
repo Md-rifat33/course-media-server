6615ab0d9a6b9ee154d61ef7fb207241f4f13d1a
Md-rifat33 <ultimaterifat@yahoo.com>
2022-10-28 21:43:56 +0600

    fetchCourseBySlug function added

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,13 +1,23 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { courseService } = require('../services');
-
+const { info } = require('../config/logger');
 
 const getAllCourse = catchAsync(async (req, res) => {
+  const { slug, id } = req.query;
+
+  if (slug) {
+    return res.json({ slug });
+  }
+
+  if (id) {
+    return res.json({ id });
+  }
+
   const data = await courseService.fetchCourses();
   res.status(httpStatus.OK).json(data);
 });
 
 module.exports = {
- getAllCourse
+  getAllCourse,
 };
